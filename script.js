608810807b6add62c98f45b12da48c8385f7553e
QusaiMaani <>
2021-05-09 12:14:17 +0000

    5

--- script.js
+++ script.js
@@ -25,9 +25,17 @@ for (var i = 0; i< players ; i++) {
 
 
 if(Fav === 'Muller'){
-  document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/0/02/Thomas_Mueller_Training_2017-05_FC_Bayern_Muenchen-3_%28cropped%29.jpg">')
+
+  function img () {
+    document.write('<img src="https://upload.wikimedia.org/wikipedia/commons/0/02/Thomas_Mueller_Training_2017-05_FC_Bayern_Muenchen-3_%28cropped%29.jpg">')
+  }
+img();
+
+
 }else if(Fav === 'Nuer'){
-  document.write('<img src="https://img.fcbayern.com/image/upload/v1601353183/cms/public/images/fcbayern-com/homepage/saison-19-20/profis/neuer/01102019_neuer.jpg">')
+  function img2 () {
+  document.write('<img src="https://img.fcbayern.com/image/upload/v1601353183/cms/public/images/fcbayern-com/homepage/saison-19-20/profis/neuer/01102019_neuer.jpg">')}
+  img2();
 }else {
 
   alert('okay you should only pick Muller or Nuer') }
